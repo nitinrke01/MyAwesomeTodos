5e5fd562a32aba783b702e2b11dcc2dc1bfabdb4
Nitin Chauhan <54750557+nitinrke01@users.noreply.github.com>
2020-04-15 22:26:27 +0530

    Update projectTodos.js

--- projectTodos.js
+++ projectTodos.js
@@ -1,37 +1,28 @@
 var ul = document.getElementById('list')
 var li;
-
 var addbutton=document.getElementById('add')
 addbutton.addEventListener('click', addItem)
-
 var removebutton=document.getElementById('remove')
 removebutton.addEventListener('click', removeItem)
-
 function addItem(){
 	var input = document.getElementById('input')
 	var item = input.value;
 	ul= document.getElementById('list')
 	var textnode = document.createTextNode(item)
-
-
 	if (item === ''){
 		return false
 		// add a p tag and assign a value of "enter your todo"
 	}
 	else {
-
 		// create li
 		li= document.createElement('li')
-
 		//create checkbox
 		var checkbox = document.createElement('input')
 	    checkbox.type = 'checkbox'
 	    checkbox.setAttribute('id','check')
-
 	    //create label
 		var label = document.createElement('label')
 		label.setAttribute('for', 'item')
-
 		// add these elements on web page 
 		ul.appendChild(label)
 		li.appendChild(checkbox)
@@ -39,15 +30,11 @@ function addItem(){
 		li.appendChild(label)
 		ul.insertBefore(li,ul.childNodes[0])
         li.className = 'visual'
-
         setTimeout(() => {
         	li.className= 'visual'
         },2);
-
         input.value= ''
-
 	}
-
 }
 function removeItem(){
 	li = ul.children
